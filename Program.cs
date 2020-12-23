d95d410b2c41657a61c9eb14fda0e670b7a4f421
Amanuel Bitsuamlak <amanbitsu34@gmail.com>
2020-12-22 22:48:12 -0800

    This one is the Master....

--- Program.cs
+++ Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using Telegram.Bot;
 
 namespace TelegramBotDev
 {
     class Program
     {
+
+        static TelegramBotClient botClient = new TelegramBotClient("1338176434:AAHj44O4diyNRxeZNH2P12qIX7A-_EIDpsI");
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            botClient.OnMessage += BotClient_OnMessage;
+            botClient.StartReceiving();
+            Console.ReadLine();
+        }
+
+        private static void BotClient_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            Console.WriteLine(e.Message.Text);
+
+
+            if (e.Message.Text == "Hi")
+            {
+                botClient.SendTextMessageAsync(e.Message.Chat.Id,"What's Up ?");
+            }
         }
     }
 }
