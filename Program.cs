6f79a20316f3113c6faf94b87611c114aa7d7a23
Amanuel Bitsuamlak <amanbitsu34@gmail.com>
2020-12-22 22:27:47 -0800

    This is the Master one . . .

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TelegramBotDev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
